4e30c603b371c8165025db813599b9d6261c71f5
Anamaria Sugar <anamaria.sgar1@gmail.com>
2018-09-03 18:00:23 +0100

    create library file

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import getDiamondShape2 from './tilee/calculator/diamond';
+import getRectangle2 from './tilee/calculator/rectangle';
+
+tilee = {};
+tilee.calculate = {
+    "diamond": getDiamondShape2,
+    "rectangle": getRectangle2
+};
